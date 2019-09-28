892b6722856845200590d865b4ae7fd168b1d4f8
zuko3 <rahulkr009@gmail.com>
2019-09-28 17:21:09 +0530

    commit

---
+++ tourist-guide-client/src/env.constants.js
@@ -0,0 +1,9 @@
+export const envConstants = {
+    BASE_URL: 'http://localhost:8000',
+};
+
+export const apiUrls = {
+    ALL_PLACES: '/admin/all-place',
+    TAGS_URL: '/admin/tags',
+    PLACE_DETAILS: '/admin/place/'
+}
