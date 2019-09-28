042dc5ff94f2adfc78e318a87418e2ddfafd5acb
zuko3 <rahulkr009@gmail.com>
2019-09-28 17:21:29 +0530

    commit

---
+++ tourist-guide-client/src/common/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Header() {
+    return (<header className="w3-container w3-center w3-padding-32">
+        <h1><b>Tourist Guide</b></h1>
+        <p>Welcome to the places of <span className="w3-tag">unknown</span></p>
+    </header>)
+}
