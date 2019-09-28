2baf00b692b878b86e7a259c7dfdfde739bf8e7e
zuko3 <rahulkr009@gmail.com>
2019-09-29 03:48:54 +0530

    sepearting place routes for user

--- tourist-guide-apis/routes/place.js
+++ tourist-guide-apis/routes/place.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const placeController = require("../controllers/place");
+const tagController = require("../controllers/tags");
 const router = express.Router();
 
+router.get("/all-place", placeController.getAllPlaces);
+router.get("/tags", tagController.getTags);
 router.get("/popular", placeController.getPopularPlace);
+router.get("/place/:id", placeController.getPlaceById);
+router.get("/alltagsplace/:tag", placeController.getPlacesByTag);
 
 module.exports = router;
