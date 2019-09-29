32fd6ab23a0654d19670b9cbab15c9f68bf33cb9
zuko3 <rahulkr009@gmail.com>
2019-09-29 22:13:29 +0530

    profile conatiner

--- tourist-guide-client/src/common/ProfileContainer.js
+++ tourist-guide-client/src/common/ProfileContainer.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { history } from "../common"
+
+function doLogout(func) {
+    typeof func === "function" && func();
+    history.push("/");
+}
 
 export function ProfileContainer(props) {
-    const { authData } = props;
+    const { authData, setLogout } = props;
     if (!authData) return;
     return (
         <div className="w3-card w3-margin">
             <div className="w3-container w3-padding" style={{ backgroundColor: '#f1f1f1' }}>
                 <h4>Profile Information</h4>
+                <span className="w3-tag" style={{ cursor: 'pointer' }}>Edit Profile</span>
+                &nbsp;&nbsp;
+                <span className="w3-tag" onClick={() => doLogout(setLogout)} style={{ cursor: 'pointer' }}>Logout</span>
             </div>
             <div className="w3-container w3-white">
                 <p><b>Name:&nbsp;</b>{authData.name}</p>
